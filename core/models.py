47b5fe66cd128eff8795b5fa10df3bceb094b80a
deannamjacobs <jacobs.deanna.m@gmail.com>
2018-12-17 13:33:02 -0500

    add classes to models

--- core/models.py
+++ core/models.py
@@ -2,5 +2,24 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
 class User(AbstractUser):
     pass
+
+
+class Snippet(models.Model):
+
+    title = models.CharField(max_length=250)
+
+    author = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="snippets"
+    )
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    language = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('created_at',)
