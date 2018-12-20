6898286f8455b2ad5a9e17a4784498bb544d97da
Sowmya <sowmya.aji@gmail.com>
2018-12-20 16:47:24 -0500

    Move dynamically loaded data to index.html from JS and API

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -1,20 +1,20 @@
-function snippetHtml(snippet) {
-    return `   
-    <div class="snippet-div">
-    <h2 class="title">${ snippet.title}</h2>
-    <h3 class="username">${ snippet.author}</h3>
-    <h4 class="language">${ snippet.language}</h4>
-    <p><pre><code class="${snippet.language}" class="card-text">${snippet.content}</code></pre></p>
-    </div>
-    `
-}
+// function snippetHtml(snippet) {
+//     return `   
+//     <div class="snippet-div">
+//     <h2 class="title">${ snippet.title}</h2>
+//     <h3 class="username">${ snippet.author}</h3>
+//     <h4 class="language">${ snippet.language}</h4>
+//     <p><pre><code class="${snippet.language}" class="card-text">${snippet.content}</code></pre></p>
+//     </div>
+//     `
+// }
 
 
-$.get('/api/snippets/').then(function (snippets) {
+// $.get('/api/snippets/').then(function (snippets) {
 
-    for (let snippet of snippets) {
-        $('#snippet-list').append(snippetHtml(snippet))
-    }
+//     for (let snippet of snippets) {
+//         $('#snippet-list').append(snippetHtml(snippet))
+//     }
 
-})
+// })
 
