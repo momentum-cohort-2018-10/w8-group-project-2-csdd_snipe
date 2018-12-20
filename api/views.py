30a62462cf0d20556822035b1cd8a243bbc4974d
Sowmya <sowmya.aji@gmail.com>
2018-12-20 13:58:24 -0500

    Add corrections to api\views.py

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.exceptions import PermissionDenied
 
 # Create your views here.
 
@@ -28,10 +29,13 @@ class MySnippetListCreateView(generics.ListCreateAPIView):
 class SnippetRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SnippetSerializer
     lookup_field = "pk"
-    # queryset = Snippet.objects.all()
+    queryset = Snippet.objects.all()
 
-    def get_queryset(self):
-        return self.request.user.snippets
+    def check_object_permissions(self, request, snippet):
+        if request.method != "GET" and snippet.author != request.user:
+            raise PermissionDenied("You are not allowed to that! Sorry!")
+
+        return super().check_object_permissions(request, snippet)
 
 
 class UserListView(generics.ListAPIView):
