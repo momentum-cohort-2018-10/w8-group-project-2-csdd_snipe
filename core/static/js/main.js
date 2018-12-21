eafd74a92676a2d71e0e43f1349d8a068ab7cbf8
cat9563 <crysatl9563@gmail.com>
2018-12-21 17:52:03 -0500

    adds ajax call to post to database

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -31,4 +31,40 @@ $("#search-button").on("click", function (event) {
 //     console.error('Trigger:', e.trigger); 
 // });
 
+// let clipboard = new ClipboardJS('.copy-button');
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
 let clipboard = new ClipboardJS('.copy-button');
+console.log(clipboard)
+
+$(".copy-button").on("click", function (clipboard){
+   console.log("hi") 
+    $.ajax({
+        type: "POST",
+        url: "/api/my_snippets/",
+        dataType: "json",
+        data: {
+            content: clipboard,
+            language: '${snippet.language}',
+            title: '${snippet.title}',
+            csrfmiddlewaretoken: csrftoken
+        }
+    })
+})
+// $('api/my_snippets').append $(clipboard);
