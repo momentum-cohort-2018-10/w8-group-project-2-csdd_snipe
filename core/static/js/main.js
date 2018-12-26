0d2620a337c75ce4df2dc4f89171a79ca167b767
Sowmya <sowmya.aji@gmail.com>
2018-12-26 09:28:41 -0500

    Some changes in template and js

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -3,19 +3,19 @@ function snippetHtml(snippet) {
     <div class="card-body">
     <h3 class="title">${snippet.title}</h3>
     <img src="https://secure.gravatar.com/avatar/${md5(snippet.author_email)}.jpg?s=150&d=mm&r=g">
-    <p class="username">Author: ${snippet.author}</p>
-    <h4 class="language">Language: ${snippet.language}</h4>
-    <p><pre><code class=${snippet.language} class="card-text">${snippet.content}</code></pre></p>
-    <button  class="fa fa-copy copy-button" style="font-size:15px;color:darkmagenta" data-language=${snippet.language} data-id=${snippet.id} data-title=${snippet.title} data-author= ${snippet.author}" data-clipboard-target="#snippet-content-${snippet.id}> Snip a Copy</button>      
-    <div class="d-flex justify-content-between align-items-center">
-    <div class="btn-group">
-        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-    </div>
-    <small class="text-muted"></small>
+    < p class="username" > Author: ${ snippet.author}</p >
+        <h4 class="language">Language: ${snippet.language}</h4>
+        <p><pre><code class=${snippet.language} class="card-text">${snippet.content}</code></pre></p>
+        <button class="fa fa-copy copy-button" style="font-size:15px;color:darkmagenta" data-language=${snippet.language} data-id=${snippet.id} data-title=${snippet.title} data-author= ${snippet.author}" data-clipboard-target="#snippet - content - ${snippet.id}> Snip a Copy</button >
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted"></small>
 
-</div>
-    `
+            </div>
+`
 }
 
 $("#search-button").on("click", function (event) {
@@ -60,9 +60,9 @@ clipboard.on("success", function (e) {
         url: "/api/my_snippets/",
         dataType: "json",
         data: {
-            content: `${snippet.content}`,
-            language: `${snippet.language}`,
-            title: `${snippet.title}`,
+            content: `${snippet.content} `,
+            language: `${snippet.language} `,
+            title: `${snippet.title} `,
             "is_copy": true,
             csrfmiddlewaretoken: csrftoken,
         }
