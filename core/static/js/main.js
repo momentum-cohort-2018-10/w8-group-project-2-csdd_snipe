ee3e47a073738aa8c5092cc9af6538423583aa05
cat9563 <crysatl9563@gmail.com>
2018-12-27 21:51:15 -0500

    adds user profile page

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -21,6 +21,11 @@ function snippetHtml(snippet) {
             </div>
 `
 }
+// function profileImage(snippet){
+//     return`
+//     <img src="https://secure.gravatar.com/avatar/${md5(snippet.author_email)}.jpg?s=150&d=mm&r=g">
+//     `
+// }
 
 function profileHtml(snippet) {
     return `   
@@ -29,17 +34,17 @@ function profileHtml(snippet) {
     <p class="username">Author:${ snippet.author}</p>
         <h4 class="language">Language: ${snippet.language}</h4>
         <p><pre><code class=${snippet.language} class="card-text">${snippet.content}</code></pre></p>
-        <button class="fa fa-copy copy-button" style="font-size:15px;color:darkmagenta" data-language=${snippet.language} data-id=${snippet.id} data-title=${snippet.title} data-author= ${snippet.author}" data-clipboard-target="#snippet - content - ${snippet.id}> Snip a Copy</button >
-            <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-muted"></small>
+    </div>
 
-            </div>
 `
 }
+
+// $.get("/api/my_snippets/").then(function (snippets){
+
+//     for (let snippet of snippets) {
+//         $('#user-pic-snips').append(profileImage(snippet))
+// }})
+
 $.get("/api/my_snippets/").then(function (snippets){
 
     for (let snippet of snippets) {
